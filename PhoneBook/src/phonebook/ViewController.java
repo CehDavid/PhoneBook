01f798a04c5a47bb490edea3ed37a6fef666679c
Sziszák <cehdavid23@gmail.com>
2021-06-10 09:51:13 +0200

    Alap grafikus beállítások

---
+++ PhoneBook/src/phonebook/ViewController.java
@@ -0,0 +1,23 @@
+
+package phonebook;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+
+public class ViewController implements Initializable {
+    
+    @FXML
+    private Label label;
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
